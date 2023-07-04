5d0e521c3b04009e7fa230404c19f968e3071cc2
SergioGonzalezB11 <sagonzalezbarajas@gmail.com>
2023-07-04 00:39:28 -0500

    l

--- Controladores.cs
+++ Controladores.cs
@@ -13,6 +13,11 @@ namespace persistencia.Entities
         public Controladores(){
             if(!ExistsFile()){
                 CrearFile();
+            }else{
+               /*  using (StreamReader reader = new StreamReader("fifawc.json"){
+                    string json = reader.ReadToEnd();
+                    this.teams = System.Text.Json.JsonSerializer.Deserialize<List<Team>>(json,new System.Text.Json.JsonSerializer{PropertyNameCaseInsensitive = true}) ?? new List<Team>(teams); */
+                }
             }
         }    
         public bool ExistsFile(){
@@ -26,15 +31,47 @@ namespace persistencia.Entities
                 return isValid;
                 
         }
-        public void CrearFile{
+        public void CrearFile(){
             if(!ExistsFile()){
                 File.Create("fifawc.json");
             }
         }
         public void AddInfo(){
             Team equipo = new Team();
-                Console.Write
+            Console.WriteLine("Ingrese el Id del equipo: ");
+            equipo.IdTeam = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre del equipo: ");
+            equipo.NameTeam = Console.ReadLine();
+            teams.Add(equipo);
+            SaveDataFile();
+        }
+        public void AddPlayerToTeam(string Id){
+            bool isAddPlayer = true;
+            Team teamToEdit =teams.FirstOrDefault(team => team.Id ?? string.Empty).equals(Id)?? new Team();
+            if (teamToEdit != null)
+            {
+                do{
+                    Player players = new Player();
+                    do{
+                        bool isValidDorsal = false;
+                        Console.WriteLine("Ingrese Nro del dorsal");
+                        isValidDorsal = (players.IdPlayer ?? string.Empty).Length > 3;
+                        if (isValidDorsal){
+                            Console.WriteLine("El numero es imposible de imprimir en la camiseta");
+                            Console.WriteLine();
+                            continue;
+                        }
+
+                    }while(GetPlayerCount(teamToEdit,(players.IdPlayer ?? string.Empty));
+                }
+            }
+
+        }
+
+        public void SaveDataFile(){
+            string json =JsonConvert.SerializeObject(teams,Formatting.Indented);
+            File.WriteAllText("fifawc.json",json);
         }
         
     }
-}
+
--- Program.cs
+++ Program.cs
@@ -8,13 +8,17 @@ internal class Program
         bool isActive = false;
         int opc = 0;
         do{
-            Console.WriteLine("1.Crear Registro\n2.Salir");
+            Console.WriteLine("1.Crear Registro\n2.Editar dato\n3.Salir");
             opc = Convert.ToInt32(Console.ReadLine());
             switch (opc){
                 case 1:
                     controller.AddInfo();
                     break;
                 case 2:
+                    Console.WriteLine("Ingrese el codigo del equipo a modificar";);
+                    controller.EditDataFile(Console.ReadLine() ?? String.Empty);
+                    break;
+                case 3:
                     isActive = false;
                     break;
                 default:
